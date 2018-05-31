a181171f19664c3eec42a1a11048d5ce0d3a208d
Christopher Beards <beardscm@gmail.com>
2018-05-31 15:45:12 -0400

    Added Styling for Notes

--- notes/src/components/Note.js
+++ notes/src/components/Note.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
+import '../styles/Note.css';
 
 class Note extends Component {
   render() {
     return (
-      <div>
-        <div>
-          {this.props.note.title} ({this.props.note.content})
+      <div className="allNotes">
+        <div className="noteCard">
+          <div className="noteTitle">
+            {this.props.note.title}
+          </div>
+          <div className="noteBody">
+            {this.props.note.content}
+          </div>
         </div>
       </div>
     )
