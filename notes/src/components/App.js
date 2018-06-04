bd5e974882012488fe628939701420cad3756a62
Christopher Beards <beardscm@gmail.com>
2018-06-04 14:29:32 -0400

    Header Created; CreateNote Created; Routing Implemented

--- notes/src/components/App.js
+++ notes/src/components/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
 import NoteList from './NoteList';
+import Header from './Header'
+import { Switch, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <NoteList />
+        <Header />
+        <div>
+          <Switch>
+            <Route exact path ="/" component={NoteList} />
+            <Route exact path ="/create" component={CreateNote} />
+          </Switch>
+        </div>
       </div>
     );
   }
---
+++ notes/src/components/CreateNote.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+
+class CreateNote extends Component {
+  state = {
+    title: '',
+    content: '',
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="flex flex-column mt3">
+          <input
+            className="mb2"
+            value={this.state.title}
+            onChange={e => this.setState({ title: e.target.value })}
+            type="text"
+            placeholder="Title"
+          />
+          <input
+            className="mb2"
+            value={this.state.content}
+            onChange={e => this.setState({ content: e.target.value })}
+            type="text"
+            placeholder="Content"
+          />
+        </div>
+        <button onClick={() => this._createNote()}>Submit</button>
+      </div>
+    )
+  }
+
+  _createNote = async () => {
+    const { title, content } = this.state
+    await this.props.postMutation({
+      variables: {
+        title,
+        content
+      }
+    })
+    this.props.history.push('/');
+  }
+}
+
+const POST_MUTATION = gql`
+  mutation PostMutation($title: String!, $content: String!) {
+    post(title: $title, content: $content) {
+      id
+      createdAt
+      title
+      content
+    }
+  }
+`
+
+export default graphql(POST_MUTATION, { name: 'postMutation' })(CreateNote)
